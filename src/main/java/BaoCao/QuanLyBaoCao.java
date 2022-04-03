3417bad7de5ef8db7e5af63f610aa62db0b6d631
Hai Phan <phanthanhhai3317@gmail.com>
2022-04-03 13:06:25 +0700

    Adding new properties danhSachThucTap, danhSachDoAn, danhSachKhoaLuan to QuanLyBaoCao class

---
+++ src/main/java/BaoCao/QuanLyBaoCao.java
@@ -0,0 +1,9 @@
+package BaoCao;
+
+import java.util.ArrayList;
+
+public class QuanLyBaoCao {
+    private ArrayList<BaoCaoThucTap> danhSachThucTap;
+    private ArrayList<BaoCaoDoAn> danhSachDoAn;
+    private ArrayList<BaoCaoKhoaLuan> danhSachKhoaLuan;
+}
