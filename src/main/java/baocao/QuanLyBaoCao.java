72b83445ec1b8d7602f049e47314383161714ec7
Hai Phan <phanthanhhai3317@gmail.com>
2022-04-09 21:20:10 +0700

    Changing QuanLyBaoCao

--- src/main/java/baocao/QuanLyBaoCao.java
+++ src/main/java/baocao/QuanLyBaoCao.java
@@ -3,51 +3,22 @@ package baocao;
 import java.util.ArrayList;
 
 public class QuanLyBaoCao {
-    private ArrayList<BaoCao> danhSachThucTap;
-    private ArrayList<BaoCao> danhSachDoAn;
-    private ArrayList<BaoCao> danhSachKhoaLuan;
+    private ArrayList<BaoCao> danhSach;
 
     public QuanLyBaoCao(){
-        this.setDanhSachThucTap(new ArrayList<>());
-        this.setDanhSachDoAn(new ArrayList<>());
-        this.setDanhSachKhoaLuan(new ArrayList<>());
+        this.setDanhSach(new ArrayList<>());
     }
 
-    public ArrayList<BaoCao> getDanhSachThucTap() {
-        return danhSachThucTap;
+    public ArrayList<BaoCao> getDanhSach() {
+        return danhSach;
     }
 
-    public void setDanhSachThucTap(ArrayList<BaoCao> danhSachThucTap) {
-        this.danhSachThucTap = danhSachThucTap;
-    }
-
-    public ArrayList<BaoCao> getDanhSachDoAn() {
-        return danhSachDoAn;
-    }
-
-    public void setDanhSachDoAn(ArrayList<BaoCao> danhSachDoAn) {
-        this.danhSachDoAn = danhSachDoAn;
-    }
-
-    public ArrayList<BaoCao> getDanhSachKhoaLuan() {
-        return danhSachKhoaLuan;
-    }
-
-    public void setDanhSachKhoaLuan(ArrayList<BaoCao> danhSachKhoaLuan) {
-        this.danhSachKhoaLuan = danhSachKhoaLuan;
-        this.danhSachKhoaLuan.add(new BaoCaoKhoaLuan());
-    }
-
-    public void them(BaoCaoKhoaLuan baoCao){
-        this.danhSachKhoaLuan.add(baoCao);
-    }
-
-    public void them(BaoCaoDoAn baoCao){
-        this.danhSachDoAn.add(baoCao);
+    public void setDanhSach(ArrayList<BaoCao> danhSach) {
+        this.danhSach = danhSach;
     }
 
     public void them(BaoCaoThucTap baoCao){
-        this.danhSachThucTap.add(baoCao);
+        this.danhSach.add(baoCao);
     }
 
     public void xoa(BaoCaoThucTap baoCao){
