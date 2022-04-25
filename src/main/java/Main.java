513aa750472fc7546d58b5f1b79daf147c4abd98
ntnq1306 <2051012099quynh@ou.edu.vn>
2022-04-25 10:27:57 +0700

    New changelist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import hoidong.HoiDong;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -14,7 +13,34 @@ public class Main {
     public static List<HoiDong> danhSachHoiDong = new ArrayList<>();
     public static List<SinhVien> danhSachSinhVien = new ArrayList<>();
     public static List<GiangVien> danhSachGiangVien = new ArrayList<>();
-    public static void main(String[] args) throws FileNotFoundException, ParseException {
+    public static List<SinhVien> csv;
+    public static GiangVien chonGiangVien() {
+        int dem = 0;
+        for (GiangVien gv : danhSachGiangVien) {
+            System.out.printf("%d : %s",(++dem), gv.getHoTen());
+        }
+        System.out.print("Nhap giang vien ban chon : ");
+        int sgv = Integer.parseInt(CauHinh.sc.nextLine());
+        for(int i = 0; i < danhSachGiangVien.size(); i++){
+            if(danhSachGiangVien.indexOf(i) == sgv)
+                return danhSachGiangVien.get(sgv);
+        }
+        return null;
+    }
+    public static List<SinhVien> chonSinhVien() {
+        for(SinhVien sv : danhSachSinhVien){
+            System.out.printf("Ma So: %d\nHo va Ten: %s\n", sv.getMaSo(), sv.getHoTen());
+        }
+        System.out.print("Nhap vao ma so cua sinh vien ban chon : ");
+        int msv = Integer.parseInt(CauHinh.sc.nextLine());
+        for(SinhVien sv : danhSachSinhVien){
+            if(sv.getMaSo() == msv) {
+                csv.add(sv);
+                return csv;
+            }
+        }
+    }
+    public static void main(String[] args) throws FileNotFoundException, ParseException, Exception {
         mainMenu();
         System.out.println("SHUTDOWN SYSTEM");
     }
@@ -31,7 +57,7 @@ public class Main {
         danhSachGiangVien.add(gv);
     }
 
-    public static void mainMenu() throws ParseException {
+    public static void mainMenu() throws ParseException, Exception{
         int choose;
         do{
             System.out.print("""
@@ -62,8 +88,38 @@ public class Main {
         }while(choose >= 1 && choose <= 6);
     }
 
-    public static void menuThucTap(){
-
+    public static void menuThucTap() throws Exception {
+        int choose;
+        do{
+            System.out.print("""
+                    1. Tao Bao Thuc Tap Moi
+                    2. Tim Kiem Bao Cao Bang Ma Bao CaoS
+                    3. Thoat chuong trinh
+                    Moi chon:\s""");
+            choose = Integer.parseInt(CauHinh.sc.nextLine());
+            switch (choose) {
+                case 1 -> {
+                    System.out.print("Nhap ten bao cao : ");
+                    String tenbc = CauHinh.sc.nextLine();
+                    GiangVien cgv = chonGiangVien();
+                    List <SinhVien> dssv = chonSinhVien();
+                    System.out.print("Chon thanh cong\nBan co muon chon them sinh vien hay khong ? Them : 1 ?  Khong : 0\n");
+                    int them = Integer.parseInt(CauHinh.sc.nextLine());
+                    if(them == 1 && dssv != null){
+                        dssv.addAll(chonSinhVien());
+                    }
+                    else{
+                        BaoCaoThucTap bctt = new BaoCaoThucTap(tenbc, cgv, dssv);
+                    }
+                }
+                case 2 -> {
+                    System.out.print("Nhap ma bao cao ban dang can tim : ");
+                    int mbc = Integer.parseInt(CauHinh.sc.nextLine());
+                    quanLy.timKiem(mbc);
+                }
+                default -> System.out.println("Ban chon thoat.\n");
+            }
+        }while(choose >= 1 && choose <= 3);
     }
 
     public static void menuDoAn(){
